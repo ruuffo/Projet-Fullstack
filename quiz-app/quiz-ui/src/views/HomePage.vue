9058b25583b0debfcf06755007920f7a1ce158b2
Aleaume Landrea <51741094+landreaa@users.noreply.github.com>
2022-12-13 14:52:04 +0100

    api + ui commencés

---
+++ quiz-app/quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Home page</h1>
+  {{ registeredScores }}
+</template>
+
+<script>
+import quizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      registeredScores : []
+    };
+  },
+  async created() {
+		console.log("Composant Home page 'created'")
+    this.registeredScores.push(0)
+  }
+};
+</script>
