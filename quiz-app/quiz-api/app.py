5df718fc4e38974979424ba5c8cf73e18048880b
Aleaume Landrea <51741094+landreaa@users.noreply.github.com>
2022-12-13 15:03:34 +0100

    Add base method get quiz info

--- quiz-app/quiz-api/app.py
+++ quiz-app/quiz-api/app.py
@@ -9,5 +9,11 @@ def hello_world():
 	x = 'world'
 	return f"Hello, {x}"
 
+
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+
+
 if __name__ == "__main__":
     app.run()
